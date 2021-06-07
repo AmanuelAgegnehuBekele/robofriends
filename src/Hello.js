673a77ae4e4d07435fdb745873b504cc64b88b3d
Amanuel Agegnehu Bekele <amanuelagegnehu2@gmail.com>
2021-06-07 09:07:17 +0300

    creating components

---
+++ src/Hello.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Hello.css';
+const Hello = (props) =>{
+   
+        return (
+        <div className="f1 tc">
+            <h1>Hello World!</h1>
+            <p>{props.greeting}</p>
+        </div>
+        );    
+  
+
+}
+
+export default Hello;
