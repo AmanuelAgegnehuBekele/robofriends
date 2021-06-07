f88c71feedad0bd80a2481d3d079ab359a214c31
Amanuel Agegnehu Bekele <amanuelagegnehu2@gmail.com>
2021-06-07 10:02:49 +0300

    Creating Card Component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
+import Card from './Card';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
+import {robots} from './robots';
 ReactDOM.render(
-  <React.StrictMode>
-   <Hello greeting={'Hello' + 'React Ninja'} />
-  </React.StrictMode>,
+  <div>
+    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+  </div>
+  ,
   document.getElementById('root')
 );
 
---
+++ src/robots.js
@@ -0,0 +1,73 @@
+ export const robots = [
+     {
+         id:1,
+         name: 'Leanne Graham',
+         username: 'Bret',
+         email: 'Sincer@april.biz'
+
+     },
+     {
+        id:2,
+        name: 'Ervin Howell',
+        username: 'Antonette',
+        email: 'Shanna@melissa.tv'
+
+    },
+    {
+        id:3,
+        name: 'Clementine Bauch',
+        username: 'Samantha',
+        email: 'Nathan@yesenia.net'
+
+    },
+    {
+        id:4,
+        name: 'Patricia Lebsack',
+        username: 'Karianne',
+        email: 'Julianne.OConner@kory.org'
+
+    },
+    {
+        id:5,
+        name: 'Chelsey Dietrich',
+        username: 'Kamren',
+        email: 'Lucio_Hettinger@annie.ca'
+
+    },
+    {
+        id:6,
+        name: 'Mrs. Dennis Schulist',
+        username: 'Leopolodo_Corkery',
+        email: 'karley_Dach@jasper.info'
+
+    },
+    {
+        id:7,
+        name: 'Kurtis Weissnat',
+        username: 'Elwyn.Skiles',
+        email: 'Telly.Hoger@billy,biz'
+
+    },
+    {
+        id:8,
+        name: 'Nicholas Runolfsdottir V',
+        username: 'Maxine_Nienow',
+        email: 'Sherwood@rosamond.me'
+
+    },
+    {
+        id:9,
+        name: 'Glenna Reichert',
+        username: 'Delphine',
+        email: 'Chaim_McDermot@dana.io'
+
+    },
+    {
+        id:10,
+        name: 'Clementia DuBuque',
+        username: 'Moriah.Stanton',
+        email: 'Rey.Padberg@karina.biz'
+
+    }
+
+ ]
