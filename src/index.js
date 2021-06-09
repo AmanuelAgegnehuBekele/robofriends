3d0e7d346af48bb750b397941c8f2317100387bc
Amanuel Agegnehu Bekele <amanuelagegnehu2@gmail.com>
2021-06-09 12:36:48 +0300

    Added Component
    Added App component and Search box component also styled it

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import CardList from './CardList';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 import {robots} from './robots';
-ReactDOM.render(
-    <CardList robots={robots}/>
-  ,
-  document.getElementById('root')
+ReactDOM.render(<App/> , document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
