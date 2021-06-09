f5b1971fbbe008970c72f18417221f23cce8854c
Amanuel Agegnehu Bekele <amanuelagegnehu2@gmail.com>
2021-06-09 13:23:29 +0300

    Final commit

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
 import  './App.css';
 
 
@@ -23,20 +24,22 @@ class App extends Component {
     }
 
     render() {
-        const filteredRobots = this.state.robots.filter(robots => {
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        const {robots, searchfield} = this.state
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
-        if(this.state.robots.length === 0){
-            return <h1>Loading...</h1>
-        } else {
-            return (
+        return !robots.length ?
+            <h1>Loading...</h1> :
+             (
                 <div className='tc'>
                     <h1 className='f1'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
-                    <CardList robots={filteredRobots}/>
+                    <Scroll>
+                        <CardList robots={filteredRobots}/>
+                    </Scroll>
                 </div>        
             );
-        }
+        
        
     }
    
